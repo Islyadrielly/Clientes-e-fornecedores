835bf9e936a4eabe64898fdd908af0a3dc7d37b4
Adrielly Isly <islyadrielly@gmail.com>
2022-03-17 18:24:04 -0300

    botoes do dialog

--- src/lib/shared/mat-dialog/shared.module.ts
+++ src/lib/shared/mat-dialog/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDialogComponent } from './mat-dialog.component'
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -13,6 +14,7 @@ import { MatDialogComponent } from './mat-dialog.component'
   imports: [
     CommonModule,
     MatDialogModule,
+    MatButtonModule,
   ],
 
   exports: [
